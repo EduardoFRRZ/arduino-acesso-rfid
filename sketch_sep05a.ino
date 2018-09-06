60372f05e26130a9f18c3f9a8d7ecabd804b0b62
willian <willian.agostini@gmail.com>
2018-09-05 21:48:23 -0300

    adicionaod ethernet

--- sketch_sep05a.ino
+++ sketch_sep05a.ino
@@ -1,21 +1,40 @@
 
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+// the dns server ip
+IPAddress dnServer(192, 168, 0, 1);
+// the router's gateway address:
+IPAddress gateway(192, 168, 0, 1);
+// the subnet:
+IPAddress subnet(255, 255, 255, 0);
+
+//the IP address is dependent on your network
+IPAddress ip(192, 168, 0, 2);
+
+EthernetClient client;
+byte server[] = { 192, 168, 0, 101 };
+
+String readString;
+String headerSeparator = "\r\n\r\n";
 
 #define SS_PIN 6
 #define RST_PIN 7
- 
+
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-MFRC522::MIFARE_Key key; 
+MFRC522::MIFARE_Key key;
 
-// Init array that will store new NUID 
+// Init array that will store new NUID
 byte nuidPICC[4];
 
-void setup() { 
+void setup() {
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
+  rfid.PCD_Init(); // Init MFRC522
 
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
@@ -24,8 +43,17 @@ void setup() {
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
   Serial.print(F("Using the following key:"));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+
+
+  // initialize the ethernet device
+  Ethernet.begin(mac, ip, dnServer, gateway, subnet);
+  //print out the IP address
+  Serial.print("IP = ");
+  Serial.println(Ethernet.localIP());
+
+
 }
- 
+
 void loop() {
 
   // Look for new cards
@@ -40,18 +68,11 @@ void loop() {
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
 
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-    }
-   
-    Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
+  Serial.print(F("In hex: "));
+  String uid = printHex(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println();
+
+
 
 
   // Halt PICC
@@ -63,21 +84,27 @@ void loop() {
 
 
 /**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
-void printHex(byte *buffer, byte bufferSize) {
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+String printHex(byte *buffer, byte bufferSize) {
+  String key;
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
+    key += buffer[i] < 0x10 ? " 0" : " ";
   }
+  return key;
 }
 
 /**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
+   Helper routine to dump a byte array as dec values to Serial.
+*/
+String printDec(byte *buffer, byte bufferSize) {
+  String key;
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], DEC);
+    key += buffer[i] < 0x10 ? " 0" : " "
   }
+  return key;
 }
